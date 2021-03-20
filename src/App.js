cbecdfccef87368baf7edc6fa57859e399eca991
Mario Hernandez <mariohernandezk10@gmail.com>
2021-03-20 17:01:59 -0500

    created the header

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import React from "react";
+import Wrapper from './components/Wrapper';
+// import Main from './components/Main';
+import Header from './components/Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header/>
+        {/* <Main/> */}
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "./Container";
+
+function Main () {
+    return (
+        <>
+        <Container/>
+        </>
+    )
+}
+
+export default Main;
